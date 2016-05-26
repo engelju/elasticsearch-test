17121024fcb05f3ab3b7cd7487a86314c01098ba
Julie Engel <jue@metanet.ch>
2016-05-26 13:21:45 +0200

    add a basic search form

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Searching with ElasticSearch</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <form action="index.php" method="GET" autocomplete="off">
+            <label>Search for something <input type="text" name="q"></label>
+            <input type="submit" value="Search">
+        </form>
+    </body>
+</html>
