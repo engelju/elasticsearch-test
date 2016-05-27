2d6a7c55b963492e9214b069830588a958b33d5a
Julie Engel <jue@metanet.ch>
2016-05-27 16:39:34 +0200

    beautified forms

--- public/add.php
+++ public/add.php
@@ -6,14 +6,20 @@ require_once '../app/init.php'
     <head>
         <meta charset="utf-8">
         <title>Adding with ElasticSearch</title>
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.1.0/milligram.min.css">
     </head>
     <body>
-        <form action="add.php" method="GET" autocomplete="off">
-            <label>Title <input type="text" name="title"></label></br>
-            <label>Body <textarea name="body" rows="8"></textarea></label></br>
-            <label>Keywords <input type="text" name="keywords" placeholder="comma, seperated, values"></label></br>
-            <input type="submit" value="Search">
-        </form>
+        <div class="container" style="padding-top:7.5rem;">
+            <h2>Add a new item to the index</h2>
+            <form action="add.php" method="POST" autocomplete="off">
+                <fieldset>
+                    <label>Title <input type="text" name="title"></label>
+                    <label>Body <textarea class="form-control" name="body" rows="8"></textarea></label>
+                    <label>Keywords <input type="text" name="keywords" placeholder="comma, seperated, values"></label>
+                    <input type="submit" value="Add" class="button-primary">
+                </fieldset>
+            </form>
+        </div>
     </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -6,12 +6,17 @@ require_once '../app/init.php'
     <head>
         <meta charset="utf-8">
         <title>Searching with ElasticSearch</title>
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.1.0/milligram.min.css">
     </head>
     <body>
-        <form action="index.php" method="GET" autocomplete="off">
-            <label>Search for something <input type="text" name="q"></label>
-            <input type="submit" value="Search">
-        </form>
+        <div class="container" style="padding-top:7.5rem;">
+            <form action="index.php" method="GET" autocomplete="off">
+                <fieldset>
+                    <label>Search for something <input type="text" name="q"></label>
+                    <input type="submit" value="Search" class="button-primary">
+                </fieldset>
+            </form>
+        </div>
     </body>
 </html>
